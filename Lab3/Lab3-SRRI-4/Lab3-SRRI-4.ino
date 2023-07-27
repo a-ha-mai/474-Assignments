73a02ca2f38d30e90f007e19e52a9520d50f974d
a-ha-mai <a.hoang.anh.mai@gmail.com>
2023-07-27 11:24:07 -0700

    pt 4 now uses SRRI scheduler, sevseg has flickering issue

--- Lab3/Lab3-SRRI-4/Lab3-SRRI-4.ino
+++ Lab3/Lab3-SRRI-4/Lab3-SRRI-4.ino
@@ -1,15 +1,6 @@
-/**
- * @file Lab3_DDS.ino
- * @date __date__
- * @brief ECE Lab 3 Assignment
- * @detail University of Washington ECE 474
- * 
- * 
- */
-// Define states for TCB, timers, and periods
-// FILL THESE VALUES ACCORDING TO THE SPECIFICATION GIVEN IN YOUR ASSIGNMENT
 #include "SevSeg.h"
 SevSeg sevseg; //Instantiate a seven segment controller object
+// Define pins and timers for LED and speaker
 #define LED_DDR   DDRL
 #define LED_PORT  PORTL
 #define LED_PIN   PL0 // pin 49
@@ -18,6 +9,9 @@ SevSeg sevseg; //Instantiate a seven segment controller object
 #define SPEAKER_DDR  DDRH
 #define SPEAKER_PIN  PH3 // pin 6
 
+#define INTERRUPT_LED_PIN PL1 // pin 48
+
+// Define note frequencies
 #define E 659
 #define C 523
 #define G 784
@@ -26,37 +20,49 @@ SevSeg sevseg; //Instantiate a seven segment controller object
 
 #define MAX_SIZE 10
 
-unsigned long timerCounter = 0;
-int currentTask = 0;
-int counterValue = 0;
+// Declare function prototypes
+void flashExternalLED();
+void playSpeaker(); 
+void updateDisplay();
+void bit_set(volatile uint8_t& reg, uint8_t bit);
+void bit_clear(volatile uint8_t& reg, uint8_t bit);
+void sleep_474(int t);
+void schedule_sync();
 
+// Task states
 enum TaskState {
   READY,
   RUNNING,
-  SLEEPING,
-  DONE,
-  PENDING,
-  DEAD
+  SLEEPING
 };
 
-/**
-  * A struct that defines the template of characteristics for one task
-  * @return a struct that contains the characteristics declared within
-  */
-struct TCB {
-  int pid;
-  void* function;
-  int state;
-  int timesRestarted;
-} TCB_struct; // name given to the TCB struct
+// Flag States
+enum FlagState {
+  PENDING,
+  DONE
+};
 
-// Create an array of TCB structures to control the processes
-struct TCB taskScheduler[MAX_SIZE];
-struct TCB deadTaskList[MAX_SIZE];
-// Here we will define our states for TCB, timers, and periods based on the specifications given in the assignment
-
-// An array of TCB structures will be created here to control the processes
-//void updateDisplay();
+// Array of function pointers to tasks to be executed
+void (*taskScheduler[MAX_SIZE])() = {flashExternalLED, playSpeaker, updateDisplay, schedule_sync}; // Adjust the array size according to the number of tasks
+// Array to keep track of task states (READY, RUNNING, SLEEPING, PENDING, DONE)
+enum TaskState taskStates[MAX_SIZE] = {READY, READY, READY, READY}; // Initialize the states for each task
+// Array to store sleep duration for each task
+int taskSleep[MAX_SIZE] = {0, 0, 0, 0};
+// Array to store the start time of sleep for each task
+unsigned long taskSleepStartTime[MAX_SIZE] = {0, 0, 0, 0};
+// Flag to signal when a task is completed
+volatile int sFlag = READY;
+// A counter that tracks how many times the ISR has run since last interupt LED toggle
+volatile long isrCounter = 0;
+// How many times the ISR should run before the interupt LED gets toggled
+const uint16_t toggleThreshold = 100;
+// Index of the current task being executed
+int currentTask = 0;
+// Variable to keep track of time
+unsigned long timerCounter = 0;
+//x will be incremented by 1 in each iteration where the program waits until sFlag changes from PENDING 
+int x = 0;
+// Setup function, runs once at the start
 void setup() {
   byte numDigits = 4;
   byte digitPins[] = {22, 23, 24, 25};
@@ -66,64 +72,72 @@ void setup() {
   bool updateWithDelays = false; // Default 'false' is Recommended
   bool leadingZeros = false; // Use 'true' if you'd like to keep the leading zeros
   bool disableDecPoint = true; // Use 'true' if your decimal point doesn't exist or isn't connected
-
-   
+  
   // Set pins as outputs to corresponding DDR
   bit_set(LED_DDR, LED_PIN);
+  bit_set(LED_DDR, INTERRUPT_LED_PIN);
   bit_set(SPEAKER_DDR, SPEAKER_PIN);
   // Set Waveform Generation bits (WGM) to Fast PWM mode to timer 4
   TCCR4A = (1 << WGM41) | (1 << WGM40);
   TCCR4B = (1 << WGM43) | (1 << WGM42);
   
-  // Set Output Compare A (OC4A) 
-  TCCR4A |= (1 << COM4A0) | (1 << COM4B0) | (1 << COM4C0);
-  TCCR4A &= ~(1 << COM4A1) | ~(1 << COM4B1) | ~(1 << COM4C1);
+  // Set Output Compare A (OC4A) for toggling on compare match
+  TCCR4A |= (1 << COM4A0);
   
   // Set Timer/Counter4 prescaler to 64 (desired frequency range)
   TCCR4B |= (1 << CS41) | (1 << CS40);
-
+  
+  // Set the timer mode and prescaler to achieve 2ms interval for ISR
+  TCCR3A = 0; // Normal operation
+  TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30); // CTC mode, prescaler 64
+  OCR3A = 500; 
+  
+  
+  // Enable TIMER3 compare match A interrupt
+  TIMSK3 |= (1 << OCIE3A);
+  
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
   updateWithDelays, leadingZeros, disableDecPoint);
   sevseg.setBrightness(100);
-    // Initialize the task for updating the display
-  taskScheduler[0].pid = 0;
-  taskScheduler[0].function = flashExternalLED;
-  taskScheduler[0].state = READY;
-  taskScheduler[0].timesRestarted = 0;
-
-  taskScheduler[1].pid = 1;
-  taskScheduler[1].function = playSpeaker;
-  taskScheduler[1].state = READY;
-  taskScheduler[1].timesRestarted = 0;
-
-  taskScheduler[2].pid = 2;
-  taskScheduler[2].function = updateDisplay;
-  taskScheduler[2].state = READY;
-  taskScheduler[2].timesRestarted = 0;
+    
+  // Enable global interrupts
+  sei();
 }
-
+// Loop function, runs repeatedly after setup
 void loop() {
-  while(1) {
-    scheduler();
+  // Enter the loop when a task is present and not null, one of the 3
+  while (taskScheduler[currentTask] != NULL) {
+    // Run the task if it is not in the SLEEPING state
+    if (taskStates[currentTask] != SLEEPING) {
+      taskScheduler[currentTask]();
+      // Change the task state to RUNNING
+      taskStates[currentTask] = RUNNING;
+    }
+    // Increment the currentTask and timerCounter
+    currentTask = (currentTask + 1) % 10;
     timerCounter++;
+    // Delay for 1ms
     delay(1);
   }
+  // Reset the currentTask to 0
+  currentTask = 0;
 }
 
-// Function for flashing the external LED
 void flashExternalLED() {
   const int onInterval = 250;
   int interval = 1000;   // Total interval (1s)
-  // The expression timerCounter % interval calculates the remainder of the division of timerCounter by interval. 
-  // This value will cycle from 0 to (interval - 1) and then back to 0.
-  if (timerCounter % interval <= onInterval) {
-    bit_set(LED_PORT, LED_PIN);
-    
-  } else {
-    bit_clear(LED_PORT, LED_PIN);
+
+  if (taskStates[currentTask] == RUNNING) {
+    if (timerCounter % interval <= onInterval) {
+      bit_set(LED_PORT, LED_PIN);
+    } else {
+      bit_clear(LED_PORT, LED_PIN);
+    }
   }
 }
+
 int songCycle() {
+  // chaning the rest element to beat of 80 which yeilds 4s pause between cycles 
   int melody[] = {E, R, E, R, R, E, R, R, C, R, E, R, R, G, R, R, R, g, R};
   int beats[]  = {5, 1, 5, 1, 5, 5, 1, 5, 5, 1, 5, 1, 5, 5, 1, 5, 5, 5, 80};
   int melodyLength = sizeof(melody) / sizeof(melody[0]);
@@ -133,95 +147,94 @@ int songCycle() {
   int noteDuration = 50 * beats[noteIndex];
   int freq = melody[noteIndex];
   int playTime;
-  
+  // Check if the current note's duration has elapsed
   if (timerCounter - currentTime >= noteDuration) {
-    noteIndex = (noteIndex + 1) % melodyLength;
+  // Move on to the next note in the melody  
+    noteIndex++;
+  // Update the current time to the current value of timerCounter
     currentTime = timerCounter;
   }
-  return freq;
-}
-
-/**
-  * Function that plays a theme from a song
-  * and then sleeps for a certain amount of time, then plays the theme again
-  */
-void playSpeaker() {
-  int freq = songCycle();
-  if (freq == 0) {
-    OCR4A = 0;
-  } else {
-    OCR4A = (F_CPU / (64UL * freq)) / 2; //The value of OCR4A is set according to the frequency returned from speakerCycle()
-  }
-}
-void function_ptr(void* function()) {
-  // Invoke the function passed as parameter here
-  function();
-}
-
-void task_self_quit() {
-  taskScheduler[currentTask].state = DEAD;
-  // Move this task to the deadTaskList
-  deadTaskList[currentTask] = taskScheduler[currentTask];
-  // Shift down the remaining tasks in taskScheduler
-  for (int i = currentTask; i < MAX_SIZE - 1; i++) {
-    taskScheduler[i] = taskScheduler[i + 1];
-  }
-  // Clear the last element of taskScheduler since it's a duplicate of the previous one
-  taskScheduler[MAX_SIZE - 1].pid = NULL;
-  taskScheduler[MAX_SIZE - 1].function = NULL;
-  taskScheduler[MAX_SIZE - 1].state = NULL;
-  taskScheduler[MAX_SIZE - 1].timesRestarted = NULL;
-}
-
-/**
-  * This function allows a task to start up another task\
-  * A task can call this function to change the status of a DEAD task to READY,\
-  * effectively "reviving" the task.\
-  * @param[task] pointer to task control block struct\
-  */  
-void task_start(TCB* task) {
-  // Find the first null entry in the taskScheduler
-  int firstNullIndex;
-  for (int i = 0; i < MAX_SIZE; i++) {
-    if (taskScheduler[i].function == NULL) {
-      firstNullIndex = i;
+  // Check if all notes in the melody have been played
+  if (noteIndex >= melodyLength) {
+    // If the end of the melody is reached, reset the noteIndex to 0 to start over  
+    
+    noteIndex = 0;
+    for (int i = 0; i < MAX_SIZE; i++) {
+    if (taskScheduler[i] == sleep_474) {
+      currentTask = i;
       break;
     }
   }
-  // Set the desired task's state to READY and move it to the taskScheduler
-  task->state = READY;
-  task->timesRestarted++;
-  taskScheduler[firstNullIndex] = *task;
-
-  // Clear the task from the deadTaskList
-  deadTaskList[currentTask].pid = NULL;
-  deadTaskList[currentTask].function = NULL;
-  deadTaskList[currentTask].state = NULL;
-  deadTaskList[currentTask].timesRestarted = NULL;
+  //sleep_474(4000); // 4-second sleep between song cycles
+}
+return freq;
 }
 
-void scheduler(){
-  while (taskScheduler[currentTask].function != NULL) {
-    if (taskScheduler[currentTask].state != SLEEPING) {
-      function_ptr(taskScheduler[currentTask].function);
-      taskScheduler[currentTask].state = RUNNING;
-    }
-    currentTask++;
+// This function is responsible for playing the notes on the speaker.
+void playSpeaker() {
+  // Extract the frequency of the current note from the songCycle() function
+  int freq = songCycle();
+  // Check if the frequency is 0, which means a rest or no note to play
+  if (freq == 0) {
+    //sleep_474(4000);
+    // Turn off the speaker by setting the Output Compare Register A to 0
+    OCR4A = 0;
+  } else {
+    // If it's not a rest, calculate the appropriate OCR4A value to produce the desired frequency
+    // Toggling the speaker on compare match, which controls the frequency of the output
+    OCR4A = (F_CPU / (64UL * freq)) / 2;
   }
-  currentTask = 0;
-}
-
-// set the bit to 1 given bit position and register name
-void bit_set(volatile uint8_t& reg, uint8_t bit) {
-  reg |= (1 << bit);
-}
-// set the bit to 0 given bit position and register name
-void bit_clear(volatile uint8_t& reg, uint8_t bit) {
-  reg &= ~(1 << bit);
 }
 
 void updateDisplay() {
   int deciSecondsElapsed = (timerCounter / 100) % 1000;
   sevseg.setNumber(deciSecondsElapsed, 0);
   sevseg.refreshDisplay(); // Must run repeatedly
+}
+
+void bit_set(volatile uint8_t& reg, uint8_t bit) {
+  reg |= (1 << bit);
+}
+
+void bit_clear(volatile uint8_t& reg, uint8_t bit) {
+  reg &= ~(1 << bit);
+}
+// This function puts the current task to sleep for a specified duration.
+// The task is marked as SLEEPING, and its sleep start time and sleep duration are recorded.
+void sleep_474(int t) {
+  // Set the task state to SLEEPING
+  taskStates[currentTask] = SLEEPING;
+  // Record the current time as the start time of sleep
+  taskSleepStartTime[currentTask] = timerCounter;
+  // Set the sleep duration for the current task
+  taskSleep[currentTask] = t;
+}
+
+ISR(TIMER3_COMPA_vect) {
+  isrCounter++; // Increment the ISR counter
+
+  if (isrCounter >= toggleThreshold) {
+      LED_PORT ^= (1 << INTERRUPT_LED_PIN);
+      isrCounter = 0; // Reset the counter after LED toggle
+  }
+  sFlag = DONE;
+}
+
+void schedule_sync() {
+  while (sFlag == PENDING) {
+    x += 1;
+  }
+  
+  unsigned long currentTime = timerCounter;
+  
+  if (taskStates[currentTask] == SLEEPING) {
+    unsigned long elapsedTime = currentTime - taskSleepStartTime[currentTask];
+    if (elapsedTime >= taskSleep[currentTask]) {
+      // If the sleep duration has passed, set the task state to READY
+      taskStates[currentTask] = READY;
+    }
+  } else {
+    taskStates[currentTask] = READY;
+  } 
+  sFlag = PENDING; // Reset sFlag to PENDING
 }
