21765e2fe709ea7b8cd9b6c575d81430bdb1f80b
Paria <76576654+PariaNaghavi@users.noreply.github.com>
2023-08-10 12:01:21 -0700

    lab4b done- comments and cleaning to do

--- Lab4B/blink_analogRead474.ino
+++ Lab4B/blink_analogRead474.ino
@@ -3,6 +3,7 @@
 #include <queue.h>
 #include <math.h>
 #include <arduinoFFT.h>
+arduinoFFT FFT;
 
 
 #define E 659
@@ -17,9 +18,13 @@
 #define FFT_SIZE 128 // Start with 128 samples
 double real[FFT_SIZE];
 double imag[FFT_SIZE];
+TaskHandle_t R3;
+TaskHandle_t R4;
 
-QueueHandle_t completionQueue;
-QueueHandle_t fftQueue;
+
+QueueHandle_t fftQueue = xQueueCreate(1, sizeof(double *));
+QueueHandle_t completionQueue = xQueueCreate(5, sizeof(int)); 
+
 double data[N]; 
 
 
@@ -27,6 +32,7 @@ double data[N];
 const int melody[] = {E, R, E, R, R, E, R, R, C, R, E, R, R, G, R, R, R, g, R};
 const int beats[]  = {5, 1, 5, 1, 5, 5, 1, 5, 5, 1, 5, 1, 5, 5, 1, 5, 5, 5, 5};
 const int melodyLength = sizeof(melody) / sizeof(melody[0]);
+const double sampleFreq = 5000;
 
 // define two tasks for Blink 
 void TaskBlink( void *pvParameters );
@@ -34,7 +40,7 @@ void TaskAnalogRead( void *pvParameters );
 void SpeakerTick(void *pvParameters);
 void TaskRT3(void *pvParameters);
 void TaskRT3p0(void *pvParameters);
-void TaskRT3p1(void *pvParameters);
+//void TaskRT3p1(void *pvParameters);
 
 
 
@@ -42,15 +48,15 @@ void TaskRT3p1(void *pvParameters);
 // the setup function runs once when you press reset or power the board
 void setup() {
     pinMode(SPEAKER_PIN, OUTPUT);
-    Serial.begin(9600);
+    Serial.begin(19200);
     while (!Serial);
 
     xTaskCreate(TaskBlink, "FlashLED", 128, NULL, 1, NULL);
     xTaskCreate(SpeakerTick, "Speaker", 128, NULL, 1, NULL);
     //xTaskCreate(TaskRT3, "RT-3", 1000, NULL, 2, NULL);
-    xTaskCreate(TaskRT3p0, "RT3p0", 2048, NULL, 2, NULL);
-    xTaskCreate(TaskRT3p1, "FFTTask1", 1024, NULL, 2, NULL);
-    //xTaskCreate(TaskRT4, "FFTTask2", 1024, NULL, 2, NULL);
+    xTaskCreate(TaskRT3p0, "RT3p0", 2048, NULL, 2, &R3);
+    //xTaskCreate(TaskRT3p1, "FFTTask1", 1024, NULL, 2, NULL);
+    xTaskCreate(TaskRT4, "FFTTask2", 2048, NULL, 2, &R4);
 
     vTaskStartScheduler();
 }
@@ -111,61 +117,64 @@ void SpeakerTick(void *pvParameters) {
 
 
 void TaskRT3p0(void *pvParameters) {
+      //Serial.print("show me RT3 works!!!!1");
+
     // Generates an array of N pseudo-random doubles
     for (int i = 0; i < N; i++) {
-        data[i] = (double)random(-1000, 1000) / 1000; // random double between -1 and 1
+        data[i] = (double)random() / RAND_MAX*(100-0)+100; 
+ 
     }
 
-    // Initialize a FreeRTOS Queue
-    fftQueue = xQueueCreate(1, sizeof(double *));
-    completionQueue = xQueueCreate(5, sizeof(int)); 
-
-    // Starts RT3p1 and halts itself
-    xTaskCreate(TaskRT3p1, "RT3p1", 2048, NULL, 2, NULL);
-    vTaskDelete(NULL);
-}
-
-void TaskRT3p1(void *pvParameters) {
-    int elapsedTime = 0;
-    int totalElapsedTime = 0;
+    TickType_t elapsedTime = 0;
+    TickType_t totalElapsedTime = 0;
 
     for (int i = 0; i < 5; i++) {
         // Send a pointer to the data to task RT-4
-        double *dataPointer = data;
-        xQueueSend(fftQueue, &dataPointer, portMAX_DELAY);
 
-        // Block and wait for the FFT to complete
-        xQueueReceive(completionQueue, &elapsedTime, portMAX_DELAY);
-        totalElapsedTime += elapsedTime;
+        if (xQueueSend(fftQueue, &data, pdMS_TO_TICKS(100)) == pdPASS) {
+           Serial.println("Data was sent to FFT ");
+           vTaskResume(R4);
+           vTaskSuspend(R3);
+
+        }
+         if  (xQueueReceive(completionQueue, &elapsedTime, portMAX_DELAY) == pdTRUE){
+
+         totalElapsedTime= totalElapsedTime+ elapsedTime;
+          Serial.println( (totalElapsedTime*1000)/configTICK_RATE_HZ);
+         }
     }
-
-    // Report back to your laptop/PC
-    Serial.print("Total wall clock time elapsed for the 5 FFTs: ");
-    Serial.print(totalElapsedTime);
-    Serial.println(" ticks");
-
     vTaskDelete(NULL);
+   
 }
-void TaskRT4(void *pvParameters) {
-    arduinoFFT FFT = arduinoFFT(); // Create FFT object
 
-    double *receivedData;
+
+void TaskRT4(void *pvParameters) {
+    //arduinoFFT FFT = arduinoFFT(); // Create FFT object
+    TickType_t comp = 0;
+    double receivedData[N];
     double real[N]; // Assuming N is the size of your data array
     double imag[N];
 
-    while (1) {
-        xQueueReceive(fftQueue, &receivedData, portMAX_DELAY);
-
-        for (int i = 0; i < N; i++) {
-            real[i] = receivedData[i];
-            imag[i] = 0; // Initialize imaginary part to zero
-        }
-
-        TickType_t startTick = xTaskGetTickCount(); // Get start tick count
-        FFT.Compute(real, imag, N, FFT_FORWARD); // Perform FFT
-        TickType_t endTick = xTaskGetTickCount(); // Get end tick count
-
-        int elapsedTime = endTick - startTick; // Calculate elapsed time
-        xQueueSend(completionQueue, &elapsedTime, portMAX_DELAY); // Send elapsed time back to RT-3
+    for (int i=0; i< N ; i++){
+      imag[i]=0.0;
     }
+
+   for (int i = 0; i < 5; i++) {
+       if  (xQueueReceive(fftQueue, &receivedData, portMAX_DELAY) == pdTRUE){
+          Serial.println("Data was recieved from Task 3 ");
+          FFT = arduinoFFT (receivedData, imag, N, sampleFreq);
+          FFT.Windowing (FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+          TickType_t T1= xTaskGetTickCount();
+          FFT.Compute(FFT_FORWARD);
+          TickType_t T2= xTaskGetTickCount();
+          comp= T2- T1;
+
+       }
+       if(xQueueSend(completionQueue, &comp, pdMS_TO_TICKS(100)) == pdPASS){
+          Serial.println("Data was sent to Task 3 ");
+          vTaskResume(R3);
+          vTaskSuspend(R4);
+
+
+         }  }
 }
